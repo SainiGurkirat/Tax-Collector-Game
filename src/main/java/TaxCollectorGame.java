a64af4325d5984388b7d27152bba4d7d0963658b
SainiGurkirat <25sainig@retsdstudent.ca>
2025-03-21 16:56:02 +0000

    needs fix
    Co-authored-by: LenardV25 <LenardV25@users.noreply.github.com>

--- src/main/java/TaxCollectorGame.java
+++ src/main/java/TaxCollectorGame.java
@@ -1,31 +1,99 @@
 import java.util.Set;
 import java.util.HashSet;
+import java.util.Scanner;
 
 
 public class TaxCollectorGame {
 
-
+private static Scanner scanner = new Scanner(System.in);
+    private static int ceilingNumber = 10; 
     public static void main(String[] args){
+        //making user call ceiling number, call game func with ceiling # (1-100)
+        System.out.print("Welcome to Tax Game \n\nRules: Pick numbers from the list to add to your score. If you pick a number that divides other numbers in the list, the Tax Collector earns those numbers. The game ends when all numbers are picked or taxed. The player with the highest score wins!");
 
-        int[] test = {100, 99, 98, 96, 94};
-
-        int[] test1 = taxCollectorChoice(test);
-
-        displayBoxes(100, test1, test);
+        System.out.print("\n\nEnter Your Ceiling Number: ");
         
-        for(int i: test1){
-            System.out.println("\n" + i);
+        while(true){
+            if(scanner.hasNextInt()){
+                ceilingNumber = scanner.nextInt();
+                scanner.nextLine();
+
+                if (ceilingNumber > 0 && ceilingNumber < 100) {
+                    break; 
+                } else {
+                    System.out.print("Please enter a number between 1 and 100: ");
+                }
+        }else{
+            System.out.print("Please Enter A Valid Number: ");
+            scanner.nextLine();
         }
+        } 
+
+        Game();
     }
-//player score
-private static int calculatePlayerScore(int[] numbers) {
+
+
+    private static int[] pickNumber(int[] taxCollectorChoice, int[] usersChoices){
+
+        int usersChoice = 0;
+        
+        System.out.print("\nEnter a number: ");
+        while(true){
+            if(scanner.hasNextInt()){
+                usersChoice = scanner.nextInt();
+                scanner.nextLine();
+
+                boolean numTaken = false; 
+                for(int taxNum: taxCollectorChoice){
+                    if(taxNum == usersChoice){
+                        System.out.print("That number is already taken! Enter new number: ");
+                        numTaken = true;
+                    }
+                }
+
+                for(int usersNum: usersChoices){
+                    if(usersNum == usersChoice){
+                        System.out.print("That number is already taken! Enter new number: ");
+                        numTaken = true;
+                    }
+                }
+
+
+                if(numTaken == true) continue;
+
+                if (usersChoice > 0 && usersChoice <= ceilingNumber) {
+                    break; 
+                } else {
+                    System.out.print("Please enter a number between 1 and " + ceilingNumber + ": ");
+                }
+        }else{
+            System.out.print("Please Enter A Valid Number: ");
+            scanner.nextLine();
+        }
+        } 
+        int[] newUsersChoices = new int[(usersChoices.length + 1)];
+
+        int index = 0;
+        for(int usersNum: usersChoices){
+            newUsersChoices[index] = usersNum;
+            index++;
+        }
+
+        newUsersChoices[index] = usersChoice;
+
+        return newUsersChoices;
+    }
+
+    //player score
+    private static int calculatePlayerScore(int[] numbers) {
         int totalscore = 0;
         for (int number : numbers) {
             totalscore += number;
         }
         return totalscore;
     }
-// tax score
+    
+    // tax score
     private static int calculateTaxscore(int[] taxnumbers) {
         int taxtotalscore = 0;
         for (int number : taxnumbers) {
@@ -34,8 +102,8 @@ private static int calculatePlayerScore(int[] numbers) {
         return taxtotalscore;
     }
 
-//finding factors of a number 
-public static Set<Integer> getFactors(int number){
+    //finding factors of a number (the user's choice)
+    public static Set<Integer> getFactors(int number){
     
         Set<Integer> factorsArray = new HashSet<>();
 
@@ -73,17 +141,46 @@ public static Set<Integer> getFactors(int number){
 
     
     //checks if game is won
-    private boolean isGameWon(int playerScore, int taxScore) {
-        if(playerScore > taxScore) {
+    private static boolean isGameWon(int playerScore, int taxScore) {
+        if(playerScore > taxScore) {    //player wins
             return true;
-        }
-        else {
-            return false;
-        }
+        } else return false;
+        
     }
 
+    private static void Game() {
+       int[] taxCollectorChoices = {};
+       int[] usersChoices = {};
+       int playerScore = 0;
+       int taxCollectorScore = 0;
+       boolean isGameWon = false;
 
-    // prints a string of all the possible option
+
+        while (true) {
+            displayBoxes(ceilingNumber, taxCollectorChoices, usersChoices);
+            usersChoices = pickNumber(taxCollectorChoices, usersChoices);
+            taxCollectorChoices = taxCollectorChoice(usersChoices);
+            
+            playerScore = calculatePlayerScore(usersChoices);
+            taxCollectorScore = calculateTaxscore(taxCollectorChoices);
+
+            if(((taxCollectorChoices.length + usersChoices.length) - 2) == ceilingNumber) {
+                break;
+            }
+        }
+        
+        isGameWon = isGameWon(playerScore, taxCollectorScore);
+        if (isGameWon == true){
+            System.out.println("You won by " + (playerScore - taxCollectorScore) + " points!");
+            
+        } else {
+            System.out.println("You lost by " + (taxCollectorScore - playerScore));
+        }
+        
+    }
+   
+    
+     // prints a string of all the possible option
     private static void displayBoxes(int ceilingNumber, int[] taxCollectorChoices, int[] usersChoices){
 
         for(int i = 1; i <= ceilingNumber; i++){
@@ -106,7 +203,40 @@ public static Set<Integer> getFactors(int number){
                     O = true;
                 }
             }
+            if(O == true){
+                // if the index reaches the end then continue to next index otherwise just continue
+                if(i == ceilingNumber){
+                    int numbersLeft = ceilingNumber%10;
+
+                    if(numbersLeft == 0) numbersLeft = 10;
+                    
+                    System.out.println();
+                    for(int s = 0; s < numbersLeft; s++){
+                        System.out.print("+===+");
+                    }
+                }
+                continue;
+            }
+
             
+            
+            // when users tax collectors choice equals the index prints out an "X" and continue to the next index
+            boolean x = false;
+            for(int taxChoice: taxCollectorChoices){
+                if(taxChoice == i){
+                    System.out.print("| X |");
+                    x = true;
+                }
+            }
+            if(x == true) continue;
+
+            //if neither tax collector choice or user choice was in the index it shuld print out the index number
+            if(i < 10){
+                System.out.print("| " + i + " |");
+             }else {
+                // fixes padding on the box when number is 2 digits big
+                System.out.print("| " + i + "|");
+            }
 
             // When the index reaches the end, print out the final set of lines at the bottom
             if(i == ceilingNumber){
@@ -119,28 +249,6 @@ public static Set<Integer> getFactors(int number){
                     System.out.print("+===+");
                 }
              }
-
-            if(O == true) continue;
-            // when users tax collectors choice equals the index prints out an "X" and continue to the next index
-            boolean x = false;
-            for(int taxChoice: taxCollectorChoices){
-                if(taxChoice == i){
-                    System.out.print("| X |");
-                    x = true;
-                }
-            }
-            if(x == true) continue;
-
-
-             //if neither tax collector choice or user choice was in the index it shuld print out the index number
-             if(i < 10){
-                System.out.print("| " + i + " |");
-             }else {
-                // fixes padding on the box when number is 2 digits big
-                System.out.print("| " + i + "|");
-            }
-            
-             
         }
    
         }
